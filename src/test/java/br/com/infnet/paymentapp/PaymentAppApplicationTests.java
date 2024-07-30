fcf502afa29a8c3208201546d554ae8555045a0e
Joao Victor Dias <kvj1610@hotmail.com>
2024-07-30 23:05:08 +0200

    Include spring boot app

---
+++ src/test/java/br/com/infnet/paymentapp/PaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infnet.paymentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
